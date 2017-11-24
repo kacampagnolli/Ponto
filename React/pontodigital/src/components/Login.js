a5bc7c68964b2ca49c860487c0f12381af341a13
leo <leobronza@hotmail.com>
2017-11-24 01:35:27 -0200

    Login Screen Button size

--- React/pontodigital/src/components/Login.js
+++ React/pontodigital/src/components/Login.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import FormButton from './utils/FormButton.js'
 import FormInput from './utils/FormInput.js'
 import Logo from './utils/Logo.js'
-import 'statics/css/Logo.css'
+import 'statics/css/Login.css'
 
 class Login extends Component {
     render() {
@@ -12,14 +12,18 @@ class Login extends Component {
             <main className="mdl-layout__content">
             <div className="login-container">
                 <div className=" mdl-card mdl-shadow--6dp">
+                <div className="logo-container">
                     <Logo/>
+                </div>
                     <div className="mdl-card__supporting-text">
                         <form action="#">
                             <FormInput labelInput='Usuário' nameInput='user' typeInput='text' idInput='user'/>
                             <FormInput msgError='' labelInput='Senha' nameInput='password' typeInput='password' idInput='password'/>
                         </form>
                     </div>
+                    <div className="mdl-card__actions mdl-card--border">
                     <FormButton textButton='Login' />
+                    </div>
                 </div>
                 </div>
                 
