4bd29cc4c4ab8d40feb9a0363ced5230abb6e83e
leo <leobronza@hotmail.com>
2017-11-23 23:00:53 -0200

    Login Screen center

--- React/pontodigital/src/components/LoginBox.js
+++ React/pontodigital/src/components/LoginBox.js
@@ -2,24 +2,30 @@ import React, { Component } from 'react';
 import FormButton from './utils/FormButton.js'
 import FormInput from './utils/FormInput.js'
 import Logo from './utils/Logo.js'
+import 'statics/css/Logo.css'
 
 class LoginBox extends Component {
     render() {
-        return (<div className="mdl-layout mdl-js-layout mdl-color--grey-100">
+        return (
+           
+        <div className=" mdl-layout mdl-js-layout mdl-color--grey-100">
             <main className="mdl-layout__content">
-                <div className="mdl-card mdl-shadow--6dp">
+            <div className="login-container">
+                <div className=" mdl-card mdl-shadow--6dp">
                     <Logo/>
                     <div className="mdl-card__supporting-text">
                         <form action="#">
-                            <FormInput textInput='text' />
-                            <FormInput textInput='password' />
+                            <FormInput labelInput='Usuário' nameInput='user' typeInput='text' idInput='user'/>
+                            <FormInput msgError='' labelInput='Senha' nameInput='password' typeInput='password' idInput='password'/>
                         </form>
                     </div>
                     <FormButton textButton='Login' />
                 </div>
+                </div>
+                
             </main>
-        </div>
-
+        
+</div>
 
         );
     }
